36aaff47341ee7258149688cc4e7ce3e3f79b0a5
fal025 <fal025@ucsd.edu>
2016-06-16 13:25:30 -0700

    my part

---
+++ getPatientId.php
@@ -0,0 +1,14 @@
+/*
+ * Name: Fangchen Li
+ * Filename: getPatientId.php
+ * Date: June 16th, 2016
+ * Description: this file contains the functionality of query with BD2k center 
+ * to get patients' Id with the given 
+ *
+<?php
+    class queryNode
+    {
+
+        
+    }
+
